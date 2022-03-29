73b08649d4ed07c91f7d7979229e11a9392106e1
texJuliana <julianateixeira150@gmail.com>
2022-03-29 18:30:25 -0300

    colocando fonts novas

--- gamePlay/App.tsx
+++ gamePlay/App.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import { Rajdhani_700Bold, Rajdhani_500Medium} from '@expo-google-fonts/rajdhani';
+
+
 import { SignIn  } from'./src/screens/SignIn';
 
 export default function App(){
